664cca0dc2c99584cdc507bc453039216d67307d
GDusek <70817559+GDusek@users.noreply.github.com>
2021-01-21 19:16:07 -0600

    Edit & Delete functions working with page refresh

--- insta-inspire-client/src/Components/EditQuote.js
+++ insta-inspire-client/src/Components/EditQuote.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import {useRef} from 'react';
 
-const EditQuoteForm = (routerProps) => {
+const EditQuoteForm = (props) => {
     const editMoodInput = useRef(null);
-    const editUserInput = useRef(null);
+    // const editUserInput = useRef(null);
     const editQuoteInput = useRef(null);
 
     const editQuote = async (event) => {
         event.preventDefault();
         const mood = editMoodInput.current.value;
-        const user = editUserInput.current.value;
-        const quote = editQuoteInput.current.value;
-        const body = JSON.stringify({mood, user, quote});
+        // const user = editUserInput.current.value;
+        const text = editQuoteInput.current.value;
+        const body = JSON.stringify({mood, text});
 
         event.currentTarget.reset();
 
         try {
-            const response = await fetch (`https://insta-api-sei-12345.herokuapp.com/quotes/${quote.id}`,
+            const response = await fetch (`https://insta-api-sei-12345.herokuapp.com/quotes/${props.match.params.id}`,
             {
                 method: 'PUT',
                 headers: {'Content-Type': 'application/json'},
@@ -24,19 +24,26 @@ const EditQuoteForm = (routerProps) => {
             }
           );
 
-          routerProps.history.push('/Home')
+        //   const data = await response.json();
+        //   const filteredQuote = props.quote.filter(
+        //       (quote) => quote._id !== data._id
+        //   );
+
+        //   props.editQuote([...filteredQuote, data]);
+            props.history.push('/')
         }catch(error) {
             console.error(error)
         }
-    }
+    };
 
     return (
         <>
         <h1>Edit Quote</h1>
             <form onSubmit={editQuote}>
-                <label>Mood:<input type='text' name='mood' ref={editMoodInput} /><br/></label>
-                <label>User:<input type='text' name='user' ref={editUserInput} /><br/></label>
                 <label>Quote:<input type='text' name='quote' ref={editQuoteInput} /><br/></label>
+                <label>Mood:<input type='text' name='mood' ref={editMoodInput} /><br/></label>
+                {/* <label>User:<input type='text' name='user' ref={editUserInput} /><br/></label> */}
+                <input type='submit' name='' value='Edit Quote'/>
             </form>   
     </>
 
